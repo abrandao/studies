57d0c0be724c18370612abde7a410b18c496100f
abrandao <andersonbrandaolustosa@gmail.com>
2024-01-10 09:49:36 -0300

    form

--- laravel/app_super_gestao/resources/views/site/layouts/_components/form_contato.blade.php
+++ laravel/app_super_gestao/resources/views/site/layouts/_components/form_contato.blade.php
@@ -2,9 +2,11 @@
 <form action={{ route('site.contato' )}} method="post">
     @csrf
     <input name="nome" value="{{ old('nome') }}" type="text" placeholder="Nome" class="{{ $classe }}">
+
     @if($errors->has('nome'))
         {{ $errors->first('nome') }}
     @endif
+
     <br>
     <input name="telefone" value="{{ old('telefone') }}" type="text" placeholder="Telefone" class="{{ $classe }}">
     {{ $errors->has('telefone') ? $errors->first('telefone') : '' }}
@@ -21,9 +23,10 @@
         @endforeach
 
     </select>
+
     {{ $errors->has('motivo_contatos_id') ? $errors->first('motivo_contatos_id') : '' }}
     <br>
-    <textarea name="mensagem" class="{{ $classe }}"> {{ (old('mensagem') != '') ? old('mensagem') :            'Preencha aqui a sua mensagem' }}
+    <textarea name="mensagem" class="{{ $classe }}"> {{ (old('mensagem') != '') ? old('mensagem') : 'Preencha aqui a sua mensagem' }}
     </textarea>
     {{ $errors->has('mensagem') ? $errors->first('mensagem') : '' }}
     <br>
