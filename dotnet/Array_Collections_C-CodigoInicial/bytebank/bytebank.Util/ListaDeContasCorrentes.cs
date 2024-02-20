af5795091bdeffc66380f95b6e7def7e2c498de4
abrandao <andersonbrandaolustosa@gmail.com>
2024-02-20 11:26:26 -0300

    Bytebank starting

---
+++ dotnet/Array_Collections_C-CodigoInicial/bytebank/bytebank.Util/ListaDeContasCorrentes.cs
@@ -0,0 +1,41 @@
+using bytebank.Modelos.Conta;
+namespace bytebank.bytebank.Util;
+
+public class ListaDeContasCorrentes
+{
+    private ContaCorrente[] _itens = null;
+    private int _proximaPosicao = 0;
+
+    public ListaDeContasCorrentes(int tamanhoInicial = 5)
+    {
+        _itens = new ContaCorrente[tamanhoInicial];
+    }
+
+    public void Adicionar(ContaCorrente item)
+    {
+        Console.WriteLine($"Adicionando item na posição {_proximaPosicao}.");
+        VerificarCapacidade(_proximaPosicao + 1);
+        _itens[_proximaPosicao] = item;
+        _proximaPosicao++;
+    }
+
+    private void VerificarCapacidade(int tamanhoNecessario)
+    {
+        if (_itens.Length >= tamanhoNecessario)
+        {
+            return;
+        }
+        else
+        {
+            Console.WriteLine("Aumentando a capacidade da lista.");
+            ContaCorrente[] novoArray = new ContaCorrente[tamanhoNecessario];
+
+            for (int i = 0; i < _itens.Length; i++)
+            {
+                novoArray[i] = _itens[i];
+            }
+
+            _itens = novoArray;
+        }
+    }
+}
